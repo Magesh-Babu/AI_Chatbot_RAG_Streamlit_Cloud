8af5ce223802f223b7a96ef1ee2c9d345a2e137f
Magesh-Babu <maheshkannan78@gmail.com>
2025-02-21 13:09:53 +0100

    added_persistent_storage

--- chat.py
+++ chat.py
@@ -2,7 +2,10 @@ import os
 import streamlit as st
 from llama_index.llms.azure_inference import AzureAICompletionsModel
 from llama_index.core import VectorStoreIndex
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+import chromadb
 
 AZURE_META_API = os.getenv("AZURE_META_API")
 AZURE_META_ENDPOINT = os.getenv("AZURE_META_ENDPOINT")
@@ -14,10 +17,18 @@ def initialize_llm():
         credential = AZURE_META_API,
     )
 
-def create_index(documents):
-    """Creates and returns a VectorStore index from the documents."""
+def connect_chromadb_create_index(documents):
+    """
+    Connects to chromaDB vector stores for persistent storage.
+    Creates and returns a VectorStore index from the documents.
+    """
+    chroma_client = chromadb.PersistentClient(path="./chroma_db")
+    chroma_collection = chroma_client.get_or_create_collection("given_doc")
     embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-    index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
+    # set up ChromaVectorStore and load in data
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, embed_model=embed_model)
     return index
 
 def display_chat():
